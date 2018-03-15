cf9f03eef3664ffcfa94e1d664c1b65d5f5e2985
aliksandrin <foxa.design@gmail.com>
2018-03-15 15:37:03 +0200

    simple date to dayOfWeek formatter with validation

---
+++ Service/src/main/java/StringValidator.java
@@ -0,0 +1,35 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class StringValidator {
+    private static final Logger logger = LoggerFactory.getLogger(StringValidator.class);
+    private static WeekDay weekDay = new WeekDay();
+
+    public String validate(String dateString) {
+        logger.info("Validation started...");
+
+        String format = "dd.MM.yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format, Locale.ENGLISH);
+        simpleDateFormat.setLenient(false);
+
+
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(dateString);
+            logger.info("Your date is valid, please wait...");
+        } catch (ParseException e) {
+            logger.error("Your string doesn't match required date format! Please try again later ;)");
+            System.exit(0);
+        }
+
+        String dayOfWeek = weekDay.get(date);
+
+
+        return dayOfWeek;
+    }
+}
---
+++ UI/src/main/java/StringReceiver.java
@@ -0,0 +1,22 @@
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StringReceiver {
+    private static final Logger logger = LoggerFactory.getLogger(StringReceiver.class);
+    private static StringValidator validator = new StringValidator();
+
+    public static void main(String[] args) {
+        // there dd.MM.yyyy must be
+        PropertyConfigurator.configure("log4j.properties");
+        if (args.length == 0) {
+            logger.error("You didn't enter an argument. Please try again later ;)");
+            System.exit(0);
+        }
+        String dateString = args[0];
+
+        logger.info(String.format("Date string %s received.", dateString));
+        String result = validator.validate(dateString);
+        logger.info(String.format("The day of the week for %s date is %s.", dateString, result));
+    }
+}
